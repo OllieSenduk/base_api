2f1ded25d384a1d6d1472e99f722755973c6703a
Oliver Senduk <oliver@ichistudios.com>
2020-07-28 08:47:46 +0200

    user model feature - adds a user class with attributes and link it to the user manager

--- app/core/models.py
+++ app/core/models.py
@@ -5,14 +5,28 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
 # User manager class - helper functions for creating users & super users
+# Used for
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """"Creates & saves a new user"""
+        if not email
+        raise ValueError("Email must be set")
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Customize the user model to support email instead of username"""
+    email = models.emailField(max_length=225, unique=True)
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
