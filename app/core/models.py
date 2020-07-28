0f3eedf4dcea5a3ab278dd34658cb38324c335cd
Oliver Senduk <oliver@ichistudios.com>
2020-07-28 08:01:03 +0200

    user feature - imports the libraries needed and creates a user manager class for creating user instances

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Needed libs to extend the user model
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+# User manager class - helper functions for creating users & super users
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """"Creates & saves a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
