b14e58eeab096f6cc934aac1dca581b483b7e515
Oliver Senduk <oliver@ichistudios.com>
2020-07-28 09:23:22 +0200

    user model refactor - use self instead of UserManager

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,7 @@ class UserManager(BaseUserManager):
         """"Creates & saves a new user"""
         if not email:
             raise ValueError("Email must be set")
-        email = UserManager.normalize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
