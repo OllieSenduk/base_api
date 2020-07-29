ebd7b89e13a18d950b3e6c8c2a4ad4441f6e7eba
Oliver Senduk <oliver@ichistudios.com>
2020-07-29 11:50:25 +0200

    admin site feature - add test file showing page contains information on admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@email.com",
+            password="pasword123"
+        )
+
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email="user@email.com",
+            password="pasword123",
+            first_name="John",
+            last_name="Smith"
+        )
+
+    def test_users_listed(self):
+        """Tests that users are listed on the user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        # assert contains checks for :
+        # http 200 requests
+        # that the page contains the content
+        self.assertContains(res, self.user.first_name)
+        self.assertContains(res, self.user.email)
