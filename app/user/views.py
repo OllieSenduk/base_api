e0e6296939766511c435244e149b7765a29d2b17
Oliver Senduk <oliver@ichistudios.com>
2020-08-06 14:27:50 +0200

    create user feature - adds views.py file for and link to to the serializers

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
