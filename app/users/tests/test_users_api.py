058baeaeb01f027c7375c85bd9974ee8a01938e0
Oliver Senduk <oliver@ichistudios.com>
2020-08-06 13:44:03 +0200

    create user feature - adds test to check for responses of password too short & user already existing

--- app/users/tests/test_users_api.py
+++ app/users/tests/test_users_api.py
@@ -22,12 +22,12 @@ class PublicUsersApiTest(TestCase):
 
     def test_create_valid_user_success(self):
         """Test creating a user with valid payload is successful"""
-        payload = (
+        payload = {
             'email': 'user@test.com',
             'password': '12345678',
             'first_name': 'John',
             'last_name': 'Smith'
-        )
+        }
 
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -35,3 +35,26 @@ class PublicUsersApiTest(TestCase):
         user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating a user that already exists"""
+        payload = {'email': 'user@test.com', 'password': '12345678'}
+
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that password must be more than 5 characters"""
+        payload = {'email': 'user@test.com', 'password': '1234'}
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+
+        self.assertFalse(user_exists)
