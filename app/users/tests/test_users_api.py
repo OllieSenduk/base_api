ce2df1b253d42dce335731f6e37abda6f032e16c
Oliver Senduk <oliver@ichistudios.com>
2020-08-06 13:55:37 +0200

    create user feature - fix params typo

--- app/users/tests/test_users_api.py
+++ app/users/tests/test_users_api.py
@@ -10,8 +10,8 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 
 
-def create_user(++params):
-    return get_user_model().objects.create_user(++params)
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUsersApiTest(TestCase):
